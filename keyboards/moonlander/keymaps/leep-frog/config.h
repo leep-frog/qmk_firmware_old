00b4572e1f376ee246928261cc680c611927e0ff
leep-frog <gleeper49@gmail.com>
2022-08-24 14:12:54 -0400

    fix combo setup

--- keyboards/moonlander/keymaps/leep-frog/config.h
+++ keyboards/moonlander/keymaps/leep-frog/config.h
@@ -2,6 +2,24 @@
 
 #define ORYX_CONFIGURATOR
 
+#include "../../config.h"
+
+// How long it takes for a tap to become a hold. Primarily added because
+// Shift-combinations were too quick and causing a parentheses to be added.
+#ifdef TAPPING_TERM
+#undef TAPPING_TERM
+#define TAPPING_TERM 150
+#endif
+// Allows us to define tapping term per key. See link for more details:
+// https://github.com/qmk/qmk_firmware/blob/master/docs/tap_hold.md#tapping-term
+#define TAPPING_TERM_PER_KEY
+
+// Combo setup
+//#define FORCE_NKRO
+#define COMBO_TERM 40  // Number of milliseconds for combo keys.
+
+#define COMBO_VARIABLE_LEN
+
 #define RGBLIGHT_LAYERS
 
 #define ENABLE_RGB_MATRIX_BREATHING
