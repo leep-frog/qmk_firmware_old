da1855076af2b14026f91c0b8432a6f0f77f29ed
leep-frog <gleeper49@gmail.com>
2022-09-23 15:12:18 -0400

    recording song

--- users/leep-frog/music.c
+++ users/leep-frog/music.c
@@ -29,9 +29,12 @@ bool _leep_mute = false;
 // All song uses should exclusively be used through references to these
 // existing songs (and not by creating new song instances).
 DEFINE_SONG(zelda_discovery_song, SONG(ZELDA_DISCOVERY));
-DEFINE_SONG(zelda_guardian_battle_song, SONG(ZELDA_GUARDIAN_BATTLE));
+// DEFINE_SONG(zelda_guardian_battle_song, SONG(ZELDA_GUARDIAN_BATTLE));
 DEFINE_SONG(zelda_item_found_song, SONG(ZELDA_ITEM_FOUND));
 DEFINE_SONG(zelda_spirit_orb_song, SONG(ZELDA_SPIRIT_ORB));
+
+DEFINE_SONG(rec_start_song, SONG(REC_START_SNG));
+DEFINE_SONG(rec_end_song, SONG(REC_END_SNG));
 
 DEFINE_SONG(mario_1_up_song, SONG(MARIO_1_UP));
 DEFINE_SONG(mario_lost_a_life_song, SONG(MARIO_LOST_A_LIFE));
@@ -43,8 +46,7 @@ DEFINE_SONG(mario_uw_2, SONG(MARIO_UNDERWORLD_2));
 #define SNG_RESET LEEP_PLAY_SONG(mario_game_over_song)
 #define SNG_MUTE LEEP_PLAY_SONG(mario_lost_a_life_song)
 #define SNG_UNMUTE LEEP_PLAY_SONG(mario_1_up_song)
-// TODO: Change this to mario underwater
-#define SNG_REC_START LEEP_PLAY_SONG(zelda_guardian_battle_song)
+#define SNG_REC_START LEEP_PLAY_SONG(rec_start_song)
 #define SNG_REC_1_END LEEP_PLAY_SONG(zelda_item_found_song)
 #define SNG_REC_2_END LEEP_PLAY_SONG(mario_1_up_song)
 #define SNG_REC_1_PLAY LEEP_PLAY_SONG(zelda_spirit_orb_song)
