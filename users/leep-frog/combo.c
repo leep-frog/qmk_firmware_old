981c3fdd4a2730df877249f735576f4c6836f97c
leep-frog <gleeper49@gmail.com>
2023-01-17 19:28:06 -0500

    fix tap dance issue

--- users/leep-frog/combo.c
+++ users/leep-frog/combo.c
@@ -29,13 +29,52 @@ const uint16_t PROGMEM df_combo[] = {KC_D, KC_F, COMBO_END};
 const uint16_t PROGMEM sd_combo[] = {KC_S, KC_D, COMBO_END};
 
 combo_t key_combos[COMBO_LENGTH] = {
-    [JI_ESC] = COMBO(ji_combo, KC_ESCAPE), [JK_HYPHEN] = COMBO(jk_combo, KC_MINUS), [JL_EQUALS] = COMBO(jl_combo, KC_EQUAL), [KL_ENTER] = COMBO(kl_combo, KC_ENTER), [DF_QUOTE] = COMBO_ACTION(df_combo), [SD_TICK] = COMBO(sd_combo, KC_GRAVE),
+    // J+I = Escape
+    [JI_ESC] = COMBO(ji_combo, KC_ESCAPE),
+    // J+K = Hyphen
+    [JK_HYPHEN] = COMBO(jk_combo, KC_MINUS),
+    // J+L = Equals
+    [JL_EQUALS] = COMBO(jl_combo, KC_EQUAL),
+    // K+L = Enter
+    [KL_ENTER] = COMBO(kl_combo, KC_ENTER),
+    // D+F = Quote
+    [DF_QUOTE] = COMBO_ACTION(df_combo),
+    // S+D = Tick (`)
+    [SD_TICK] = COMBO(sd_combo, KC_GRAVE),
 };
+
+static void end_tap_dance(bool pressed) {
+    keyevent_t leep_event = (keyevent_t){
+        // Pressed is used by preprocess_tap_dance and we need it to be true, otherwise nothing happens.
+        // If statement earlier ensures this.
+        .pressed = pressed,
+        // Key isn't used by preprocess_tap_dacne, so it can be anything. This value was just copy and pasted from tmk_core/common/keyboard.h
+        .key = (keypos_t){.row = 255, .col = 255},
+        // Time isn't used by preprocess_tap_dacne, so it can be anything.
+        .time = 0,
+    };
+    keyrecord_t leep_record = {.event = leep_event};
+    preprocess_tap_dance(0, &leep_record);
+}
 
 void process_combo_event(uint16_t combo_index, bool pressed) {
     if (!pressed) {
         return;
     }
+
+    // Tap dances aren't interrupted properly in this function because this
+    // function is run during preprocess_* (and does not advance to process_*).
+    // Meanwhile, tap dance termination is done in the process_* logic. To
+    // ensure any open tap dances are properly terminated, we explicitly end
+    // them here.
+    // Specific error that this fixes:
+    // - Press tap dance key (e.g. "i")
+    // - Before tap dance termination, activate combo that runs COMBO_ACTION (e.g. "DF_QUOTE")
+    //   (note this isn't an issue for other combos that just run COMBO(...))
+    // - Combo action will execute before tap dance does resulting in ("i) even
+    //   though we expect to get (i").
+    end_tap_dance(pressed);
+
     switch (combo_index) {
         case DF_QUOTE:
             // See below link for more info on get_mods and mod masks.
