546af74f7dc299aa29fe4ac8c7a749a91d0a2bde
leep-frog <gleeper49@gmail.com>
2023-01-20 17:29:58 -0500

    account for tap dance keys in symbol layer switch

--- users/leep-frog/features/symbol_layer_overlap.c
+++ users/leep-frog/features/symbol_layer_overlap.c
@@ -15,8 +15,9 @@ This feature file implements a fix for this issue.
 
 Test cases:
 1. See above sequence
-2. Press a key in the symbol layer
-3. Symbol key press, press 1, press 2, press 3, unpress 1, unpress 2, unpress 3, unpress symbol key.
+2. See above sequence, but with a tap dance key in the non-symbol layer (e.g. "a/&" key).
+3. Press a key in the symbol layer
+4. Symbol key press, press 1, press 2, press 3, unpress 1, unpress 2, unpress 3, unpress symbol key.
    This tests the issue where we resolve the first_symb_press due to another key being pressed (as
    opposed to unpressing the first_symb_press).
 */
@@ -52,9 +53,25 @@ bool SymbolLayerOverlap_handled(uint16_t keycode, keyrecord_t *record) {
         if (!symb_layer) {
             SEND_STRING(" ");
         }
+
         // Send the key we didn't press yet.
-        tap_code16(keymap_key_to_keycode(get_highest_layer(layer_state), first_symb_press_key));
-        // We don't want to
+        uint16_t actual_keycode = keymap_key_to_keycode(get_highest_layer(layer_state), first_symb_press_key);
+        if (actual_keycode >= QK_TAP_DANCE && actual_keycode <= QK_TAP_DANCE_MAX) {
+            // If key in other layer is a tap dance (but in this layer is just a regular key),
+            // then we need to execute the press and unpress logic for it.
+            bool original_press   = record->event.pressed;
+            record->event.pressed = true;
+            preprocess_tap_dance(actual_keycode, record);
+            process_tap_dance(actual_keycode, record);
+            record->event.pressed = false;
+            preprocess_tap_dance(actual_keycode, record);
+            process_tap_dance(actual_keycode, record);
+            record->event.pressed = original_press;
+        } else {
+            // Otherwise, just press the regular keycode
+            tap_code16(actual_keycode);
+        }
+
         return (first_symb_press_key.col == record->event.key.col && first_symb_press_key.row == record->event.key.row);
     }
 
